dc0d2ed2e15487217439e57127dbee3f31a4fc68
Count Dracul Yeamin the Scorpio <34183654+mushfiqur47@users.noreply.github.com>
2024-01-15 10:53:33 +0600

    Update Diagonal Difference.cpp

--- Diagonal Difference.cpp
+++ Diagonal Difference.cpp
@@ -1,1 +1,18 @@
+#include <bits/stdc++.h>
 
+using namespace std;
+
+int main()
+{
+    int n, item, Dl = 0, Dr = 0;
+    cin >> n;
+    for(int row = 0; row < n; row ++) {
+        for(int col = 0; col < n; col ++) {
+            cin >> item;
+            if(row == col) Dl += item;
+            if(row + col == n - 1) Dr += item;
+        }
+    }
+    cout << abs(Dl - Dr);
+    return 0;
+}
