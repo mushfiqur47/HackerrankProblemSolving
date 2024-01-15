6ee8bd9954b3a70486dbe3524f9ab92d9fd06278
Count Dracul Yeamin the Scorpio <34183654+mushfiqur47@users.noreply.github.com>
2024-01-15 10:58:53 +0600

    Update Staircase.js

--- Staircase.js
+++ Staircase.js
@@ -1,1 +1,33 @@
 
+
+function processData(input) {
+    
+    var steps = parseInt(input);
+    
+    for (var i = 0; i++ < steps; ) {
+        
+        var line = '';
+        var spaces = steps - i;
+        
+        for (var j = spaces; j--; ) {
+            line += ' ';
+        }
+        
+        for (var j = i; j--; ) {
+            line += '#';
+        }
+        
+        console.log(line);
+    }
+} 
+
+process.stdin.resume();
+process.stdin.setEncoding("ascii");
+_input = "";
+process.stdin.on("data", function (input) {
+    _input += input;
+});
+
+process.stdin.on("end", function () {
+   processData(_input);
+});
